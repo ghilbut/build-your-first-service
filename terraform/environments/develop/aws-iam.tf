8f252ebdd3bf41fc42714df10ac05f25d6bfd4d7
ghilbut <ghilbut@gmail.com>
2020-03-11 18:50:20 +0900

    Issue #10 Deploy Django with Github Actions (#19)
    * Create github actions workflow to deploy django to ECS Fargate on dev stage
    * Create secrets to access MySQL databse from django (#18)

--- terraform/environments/develop/aws-iam.tf
+++ terraform/environments/develop/aws-iam.tf
@@ -20,3 +20,34 @@ resource aws_iam_role_policy_attachment ecsTaskExecutionRole_policy {
   role       = aws_iam_role.ecsTaskExecutionRole.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+
+# https://aws.amazon.com/ko/premiumsupport/knowledge-center/ecs-data-security-container-task/
+resource aws_iam_policy secret_access_policy {
+  name        = "test-policy"
+  description = "A test policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ssm:GetParameters",
+        "secretsmanager:GetSecretValue"
+      ],
+      "Resource": [
+        "arn:aws:ssm:ap-northeast-2:*:parameter/*",
+        "arn:aws:secretsmanager:ap-northeast-2:*:secret:*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource aws_iam_role_policy_attachment secrets {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = aws_iam_policy.secret_access_policy.arn
+}
